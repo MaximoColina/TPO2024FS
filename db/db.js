9ffc116b346573cb836dc89d101f59297dcf1b1a
maximo <2mxmcln@gmail.com>
2024-06-30 22:41:40 -0300

    db en clever cloud

--- db/db.js
+++ db/db.js
@@ -2,11 +2,22 @@ const {createPool} = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT, 
+
+// variables orignales    
+//    host: process.env.DB_HOST,
+//    user: process.env.DB_USER,
+//    password: process.env.DB_PASSWORD,
+//    database: process.env.DB_DATABASE,
+//    port: process.env.DB_PORT, 
+
+
+
+// variables desde Clever Cloud
+    host: process.env.MYSQL_ADDON_HOST,
+    user: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    database: process.env.MYSQL_ADDON_DB,
+    port: process.env.MYSQL_ADDON_PORT, 
     waitForConnections: true,
     connectionLimit: 5
 });
